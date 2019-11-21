12aafdce045b5e0766b32231ae0c36f599afebd2
imd <olegfafurin1@gmail.com>
2019-11-22 01:50:29 +0300

    add 2-3-grams extractor

---
+++ makestat.py
@@ -0,0 +1,36 @@
+import sys, re, itertools as it
+
+if len(sys.argv) < 2:
+	print("USAGE: python makestat.py <filename> <n>")
+	exit(0)
+
+letters = [chr(o) for o in range(ord('a'), ord('z') + 1)]
+letters.extend([' ', '-'])
+threegrams = {a : 0 for a in list(map(lambda x: "".join(x), it.product(letters, letters, letters)))}
+bigrams = {a : 0 for a in list(map(lambda x: "".join(x), it.product(letters, letters)))}
+
+def get3grams(w):
+	word = " " + w + " "
+	for i in range(0, len(word) - 2):
+		if word[i:i+3] in threegrams:
+			threegrams[word[i:i+3]] += 1
+	for i in range(0, len(word) - 1):
+		if word[i:i+2] in bigrams:
+			bigrams[word[i:i+2]] += 1
+
+
+
+filename = sys.argv[1]
+text = open(filename, "r", encoding = "utf-8").read()
+for word in list(map(str.lower, re.findall("\w+", text))):
+	get3grams(word)
+
+g = open(filename[:filename.rfind('.')]  + "3.stat", "w+")
+for entry in threegrams:
+	g.write(entry + ": " + str(threegrams[entry]) + "\n")
+g.close()
+
+g = open(filename[:filename.rfind('.')] + "2.stat", "w+")
+for entry in bigrams:
+	g.write(entry + ": " + str(bigrams[entry]) + "\n")
+g.close()
